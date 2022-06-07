d9dd7c3571e11ef6fc58f6a0c6536ebc1137e2c0
Christian Adrian Bernal <christian.adrian.bernal@gmail.com>
2022-06-07 20:16:47 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,91 @@
+plataChris = prompt("cuanta plata tenes Chris?");
+plataJessy = prompt("cuanta plata tenes Jessy");
+plataZoe = prompt("cuanta plata tenes Zoe");
+
+plataChris = parseInt(plataChris)
+
+if (plataChris >= .6 && plataChris < 1) {
+    alert("Chris, comprate el helado de agua");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataChris >= 1 && plataChris < 1.6) {
+    alert("Chris, comprata el helado de crema");
+    alert("y te sobra" + (plataChris - 1));
+}
+else if (plataChris >= 1.6 && plataChris < 1.7) {
+    alert("Chris, comprata el helado deheladix");
+    alert("y te sobra" + (plataChris - 1.6));
+}
+else if (plataChris >= 1.7 && plataChris < 1.8) {
+    alert("Chris, comprata el helado de heladovich");
+    alert("y te sobra" + (plataChris - 1.7));
+}
+else if (plataChris >= 1.8 && plataChris < 2.9) {
+    alert("Chris, comprata el helado de helardo");
+    alert("y te sobra" + (plataChris - 1.8));
+}
+else if (plataChris >= 2.9) {
+    alert("Chris, comprata el helado con confites o el pote de 1/4Kg");
+    alert("y te sobra" + (plataChris - 2.9));
+}else {
+    alert("lo siento, no te alcanza");
+}
+
+
+if (plataJessy >= .6 && plataJessy < 1) {
+    alert("Jessy, comprata el helado de agua");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataJessy >= 1 && plataJessy < 1.6) {
+    alert("Jessy, comprata el helado de crema");
+    alert("y te sobra" + (plataChris - 1));
+}
+else if (plataJessy >= 1.6 && plataJessy < 1.7) {
+    alert("Jessy, comprata el helado deheladix");
+    alert("y te sobra" + (plataChris - 1.7));
+}
+else if (plataJessy >= 1.7 && plataJessy < 1.8) {
+    alert("Jessy, comprata el helado de heladovich");
+    alert("y te sobra" + (plataChris - 1.7));
+}
+else if (plataJessy >= 1.8 && plataJessy < 2.9) {
+    alert("Jessy, comprata el helado de helardo");
+    alert("y te sobra" + (plataChris - 1.8));
+}
+else if (plataJessy >= 2.9) {
+    alert("Jessy, comprata el helado con confites o el pote de 1/4Kg");
+    alert("y te sobra" + (plataChris - 2.9));
+} 
+
+else{
+    alert("lo siento, no te alcanza");
+}
+
+if (plataZoe >= .6 && plataZoe < 1) {
+    alert("Zoe, comprata el helado de agua");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataZoe >= 1 && plataZoe < 1.6) {
+    alert("Zoe, comprata el helado de crema");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataZoe >= 1.6 && plataZoe < 1.7) {
+    alert("Zoe, comprata el helado deheladix");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataZoe >= 1.7 && plataZoe < 1.8) {
+    alert("Zoe, comprata el helado de heladovich");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataZoe >= 1.8 && plataZoe < 2.9) {
+    alert("Zoe, comprata el helado de helardo");
+    alert("y te sobra" + (plataChris - .6));
+}
+else if (plataZoe >= 2.9) {
+    alert("Zoe, comprata el helado con confites o el pote de 1/4Kg");
+    alert("y te sobra" + (plataChris - 2.9));
+} 
+
+else{
+    alert("lo siento, no te alcanza");
+}
